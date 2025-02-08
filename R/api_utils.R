566d1b18205a4e717851e517ad43e4a97344b21d
Nicolas Casajus <rdev.nc@gmail.com>
2025-02-08 16:21:16 +0100

    feat: base url of apis

---
+++ R/api_utils.R
@@ -0,0 +1,9 @@
+#' Base URL of the Geoservices APIs provided by IGN
+#' 
+#' @description
+#' Main documentation: 
+#' https://geoservices.ign.fr/documentation/services/services-geoplateforme
+#' 
+#' @noRd
+
+gs_base_url <- function() "https://data.geopf.fr"
